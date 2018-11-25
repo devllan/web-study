c749103b9a15985173f440fd93b82cc1b6d8cc28
wjk <807648615@qq.com>
2018-11-25 14:38:44 +0800

    webpack配置

---
+++ webpack-test/src/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="text">{{text}}</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      text: "abc12"
+    };
+  }
+};
+</script>
+<style>
+#text {
+  color: red;
+}
+</style>
+
---
+++ webpack-test/webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+// const {
+//     VueLoaderPlugin
+// } = require('vue-loader')
+
+const isDve = process.env.NODE_ENV === 'development'
+
+const config = {
+    target: 'web',
+    mode: "development",
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    // plugins: [
+    //     new VueLoaderPlugin()
+    // ],
+    module: {
+        rules: [{
+                test: /\.vue$/,
+                use: [
+                    'vue-loader',
+                    // 'css-loader',
+                    // 'style-loader',
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+
+                ]
+            },
+            {
+                test: /\.(jpg|gif|png|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1024,
+                        name: '[name].[ext]'
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: isDve ? '"development"' : '"production"'
+            }
+        }),
+        new HTMLPlugin()
+    ]
+}
+
+if (isDve) {
+    config.devtool = '#cheap-moudle-eval-source-map'
+    config.devServer = {
+        port: 8000,
+        host: '0.0.0.0',
+        overlay: {
+            errors: true,
+        },
+        hot:true
+    }
+    config.plugins.push(
+        //即时加载
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    )
+}
+module.exports = config
